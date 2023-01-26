8a6c2c973e9b1147aa7f57061fadfae8844007e5
Luiz Marangoni <lzmarangoni@gmail.com>
2023-01-26 15:19:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CardFront from './components/CardFront';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardFront/index.js
@@ -0,0 +1,11 @@
+import './CardFront.css'
+
+const CardFront = ()=>{
+    return(
+        <div className='cardFront'>
+            <p>16656556565658848484</p>
+        </div>
+    )
+}
+
+export default CardFront
