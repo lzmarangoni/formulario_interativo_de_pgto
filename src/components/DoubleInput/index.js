80e2f82d4bfbb25938d278cff3a1f0a76c254a3c
Luiz Marangoni <lzmarangoni@gmail.com>
2023-03-13 00:14:01 -0300

    cards ajustados a tela de acordo com design mobile

--- src/components/DoubleInput/index.js
+++ src/components/DoubleInput/index.js
@@ -3,10 +3,10 @@ import './DoubleInput.css'
 const DoubleInput = (props)=>{
     return(
         <div className='doubleInput'>
-            <label>Vencimento do cartão</label>
-            <div>
-                <input onChange={props.captureMonth}/>
-                <input onChange={props.captureYear} style={{marginLeft:'.5rem'}}/>
+            <label>Venc. do cartão</label>
+            <div >
+                <input placeholder="MM" onChange={props.captureMonth}/>
+                <input placeholder="AAAA"onChange={props.captureYear} style={{marginLeft:'.5rem'}}/>
             </div>
         </div>
     )
